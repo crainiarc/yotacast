208559274ac1464b9d9d137e28388e152a0ae6f3
Keng Kiat Lim <kkin@crnrc.com>
2014-12-06 17:46:48 +0800

    Added video stream to the site

--- web/app/static/js/main.js
+++ web/app/static/js/main.js
@@ -7,6 +7,38 @@ function CameraController ($scope, $http) {
     max: 50
   });
 
+  // Set up the camera
+  (function() {
+    video = document.querySelector('.video-container');
+    canvas = document.querySelector('.hidden-canvas');
+    photo = document.querySelector('.hidden-photo');
+    width = 970;
+    
+    navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+    navigator.getMedia({video: true}, function(stream) {
+      if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream;
+      } else {
+        var vendorURL = window.URL || window.webkitURL;
+        video.src = vendorURL.createObjectURL(stream);
+      }
+      video.play();
+    }, function (err) { console.log('An error occured: ' + err) });
+
+    // video.addEventListener('canplay', function(ev){
+    //   if (!streaming) {
+    //     height = video.videoHeight / (video.videoWidth/width);
+    //     video.setAttribute('width', width);
+    //     video.setAttribute('height', height);
+    //     canvas.setAttribute('width', width);
+    //     canvas.setAttribute('height', height);
+    //     streaming = true;
+    //   }
+    // }, false);
+  })();
+
   $scope.uploadImage = function () {
     $http.post('/upload', {
       image: 'Yo Waihon'
